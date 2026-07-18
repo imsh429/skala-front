65f98d8d64b340679afd389cd4e3f04a2cd13a16
imsh429 <sh99429@naver.com>
2026-07-19 02:28:50 +0900

    bag js 수정

--- script/bag.js
+++ script/bag.js
@@ -1,17 +1,24 @@
 function showMyBag(){
     var myBag = [
-        { name: "옷", count: 5 },
-        { name: "신발", count: 3 },
-        { name: "책", count: 10 },
-        { name: "노트북", count: 1 }    
+        { name: "보조배터리", count: 1 },
+        { name: "화장품", count: 3 },
+        { name: "전자제품", count: 5 },
+        { name: "양말", count: 8 }    
     ]
 
     var totalItems = 0;
+    var totalResult = "";
+
+    totalResult += "[🎒 내 가방 속 물품 목록]\n-----------------------------\n";
 
     for(var i=0; i<myBag.length; i++){
         console.log("아이템: " + myBag[i].name + ", 개수: " + myBag[i].count);
         totalItems += myBag[i].count;
+        totalResult += myBag[i].name + " : " + myBag[i].count + "개" +"\n";
     }
 
-    alert("내 가방 정보:\n" + "총 아이템 개수: " + totalItems);
+    totalResult += "-----------------------------\n";
+    totalResult += "총 아이템 개수: " + totalItems + "개";
+
+    alert(totalResult);
 }
